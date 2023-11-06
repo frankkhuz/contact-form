5bd4bc997d3a573f5206457d7ec2c2d9bd183cf7
Franklin <frankkhuzy@gmail.com>
2023-11-05 16:00:12 -0800

    .

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const messageInput = document.getElementById("message");
 
-function sendEmail(){
- var params = {
-  name:document.getElementById('name').value,
-  email:document.getElementById('email').value,
-  number:document.getElementById('phone').value,
-  message:document.getElementById('message').value
- };   
+// function sendEmail(){
 
-const serviceID = "service_18qi5so"
-const templateID = "template_hb920lp"
+// }
+form.addEventListener("submit", subitForm);
+function subitForm(e) {
+  e.preventDefault();
+  var params = {
+    name: nameInput.value,
+    email: emailInput.value,
+    number: phoneInput.value,
+    message: messageInput.value,
+  };
 
-emailjs.send(serviceID, templateID, params).then((res) => {
- document.getElementById('name').value = "";
- document.getElementById('email').value = "";
- document.getElementById('phone').value = "";
- document.getElementById('message').value = "";
- console.log(res);
- alert('your message was sent successfully')
-})
-.catch((err)=>console.log(err));
+  const serviceID = "service_18qi5so";
+  const templateID = "template_hb920lp";
 
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("message").value = "";
+      console.log(res.status);
+      alert("your message was sent successfully");
+    })
+    .catch((err) => console.log(err));
 }
